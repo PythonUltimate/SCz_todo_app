53756a0ac82b2e3082ee88f4fc5149f1a0601d30
SlawCzech <slaw.czech@gmail.com>
2022-10-31 17:52:39 +0100

    marking one task as done

--- todo_app.py
+++ todo_app.py
@@ -34,12 +34,26 @@ def delete_task(task_list, task_name):
     return task_list
 
 
+def mark_one_done(task_list, task_name):
+    task = get_task(task_list, task_name)
+
+    if task is None:
+        raise ValueError(f'The task {task_name} does not exist.')
+
+    task['is_done'] = True
+
+    return task_list
+
+
 task_instance = todo()
 
 print(task_instance(add_task, task={'name': 'write paper', 'is_done': False}))
 print(task_instance(add_task, task={'name': 'prepare lecture', 'is_done': False}))
 print(task_instance(add_task, task={'name': 'examination pending', 'is_done': False}))
+print(task_instance(add_task, task={'name': 'get some rest', 'is_done': False}))
 
 print(task_instance(delete_task, task_name='examination pending'))
+
+print(task_instance(mark_one_done, task_name='get some rest'))
 
 
