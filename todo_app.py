f65506eb08fe14470afad1465105207d8c7a3dd1
SlawCzech <slaw.czech@gmail.com>
2022-11-01 11:48:30 +0100

    task counter, print messages

--- todo_app.py
+++ todo_app.py
@@ -2,7 +2,18 @@ def todo():
     task_list = []
 
     def inner(fn, **kwargs):
-        return fn(task_list, **kwargs)
+        result = fn(task_list, **kwargs)
+        counter = 0
+        for task_instance_ in task_list:
+            if not task_instance_['is_done']:
+                counter += 1
+        if counter == 1:
+            print(f'You have 1 task to do.')
+        elif counter == 0:
+            print('You have no tasks at this moment.')
+        else:
+            print(f'You have {counter} tasks to do.')
+        return result
 
     return inner
 
@@ -14,6 +25,7 @@ def filter_active_tasks(task_list):
         if not task_instance_['is_done']:
             new_task_list.append(task_instance_)
     print('Your active tasks: ')
+    print(new_task_list)
     return new_task_list
 
 
@@ -24,6 +36,7 @@ def filter_completed_tasks(task_list):
         if task_instance_['is_done']:
             done_task_list.append(task_instance_)
     print('Your have completed these tasks: ')
+    print(done_task_list)
     return done_task_list
 
 
@@ -40,6 +53,7 @@ def get_task(task_list, task_name):
 
 def add_task(task_list, task):
     task_list.append(task)
+    print('Task added.')
     return task_list
 
 
@@ -50,7 +64,7 @@ def delete_task(task_list, task_name):
         raise ValueError(f'The task {task_name} does not exist.')
 
     task_list.remove(task)
-
+    print(f'Task "{task_name}" has been deleted.')
     return task_list
 
 
@@ -68,22 +82,22 @@ def toggle_task_done(task_list, task_name):
 def mark_all_as_done(task_list):
     for task_instance_ in task_list:
         task_instance_['is_done'] = True
-
+    print('You have completed all you tasks.')
     return task_list
 
 
 task_instance = todo()
 
-print(task_instance(add_task, task={'name': 'write paper', 'is_done': False}))
-print(task_instance(add_task, task={'name': 'prepare lecture', 'is_done': False}))
-print(task_instance(add_task, task={'name': 'examination pending', 'is_done': False}))
-print(task_instance(add_task, task={'name': 'get some rest', 'is_done': True}))
+task_instance(add_task, task={'name': 'write paper', 'is_done': False})
+task_instance(add_task, task={'name': 'prepare lecture', 'is_done': False})
+task_instance(add_task, task={'name': 'examination pending', 'is_done': False})
+task_instance(add_task, task={'name': 'get some rest', 'is_done': True})
 
-print(task_instance(delete_task, task_name='examination pending'))
+task_instance(delete_task, task_name='examination pending')
 
-print(task_instance(toggle_task_done, task_name='get some rest'))
-print(task_instance(toggle_task_done, task_name='get some rest'))
+# print(task_instance(toggle_task_done, task_name='get some rest'))
+# print(task_instance(toggle_task_done, task_name='get some rest'))
 
-print(task_instance(filter_active_tasks))
-print(task_instance(filter_completed_tasks))
-# print(task_instance(mark_all_as_done))
+task_instance(filter_active_tasks)
+task_instance(filter_completed_tasks)
+# task_instance(mark_all_as_done)
